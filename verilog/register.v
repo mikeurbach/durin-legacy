d11082f4cb605d08ab446e4d83c9f011e5577bd6
Mike Urbach <mikeurbach@gmail.com>
2014-06-17 20:36:15 -0700

    so iverilog isn't working on this

--- verilog/register.v
+++ verilog/register.v
@@ -1,8 +1,10 @@
+`timescale 1 ns / 100 ps
+
 module Register(I, Q, clk, ld, rst);
    input [63:0] I;
-   input clk, ld, rst;
+   input 	clk, ld, rst;
    output [63:0] Q;
-   reg [63:0] Q;
+   reg [63:0] 	 Q;
 
    // Initialize register to NULL
    initial
@@ -28,5 +30,40 @@ module Register(I, Q, clk, ld, rst);
 endmodule // Register
 
 module RegisterTest;
+   reg [63:0] I;
+   reg 	      clk, ld, rst;
+   wire [63:0] Q;
+   
+   // Instantiate one register and a clock
+   Register reg(I,Q,clk,ld,rst);
+   Clock clock(clk);
+   
+   initial
+     begin
+	ld <= 0;
+	rst <= 0;
+	I <= 0;
+	#10;
+	I <= 24;
+	#10;
+	ld <= 1;
+	#10;
+	ld <= 0;
+	I <= 0;
+	#5;
+	rst <= 1;
+     end
+
+   initial
+     begin
+	$monitor($time, "ld: %b, rst: %b, I: %d, Q: %d",
+		 ld, rst, I, Q);
+     end
+
+   initial  
+     begin
+	$dumpfile("register.vcd");
+	$dumpvars;
+     end 
    
 endmodule; // RegisterTest
